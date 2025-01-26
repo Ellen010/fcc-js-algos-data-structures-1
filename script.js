968275baaee3c86505ecae93e837b6a68c33a9bd
Elena <157579304+Ellen010@users.noreply.github.com>
2025-01-26 10:33:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const character = "#";
 const count = 8;
 const rows = [];
+let inverted = true;
 
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-// TODO: use a different type of loop
-/*for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
-}*/
-
-let continueLoop = false;
-let done = 0;
-
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
 
 let result = ""
 
